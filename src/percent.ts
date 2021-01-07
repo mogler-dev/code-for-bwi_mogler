ab1ced777d08b59cf52de4d3da7f496874309781
Christopher Mogler <christopher@mogler.dev>
2021-01-07 08:33:51 +0100

    Added GPL 3.0 LICENSE Header

--- src/percent.ts
+++ src/percent.ts
@@ -1,4 +1,3 @@
-
 /*
     Copyright (C) 2021 Christopher Rudolf Karl-Heinz Mogler
 
@@ -15,23 +14,3 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-/**
-    -----------------------------------------
-    Was ist die optimale Beladung (Summe der Nutzwerte), wenn die beiden Transporter jeweils einmal fahren können? 
-     Erstelle einen Algorithmus, der die bestmögliche Ladeliste für jeden der beiden Transporter ermittelt.
-
-    -----------------------------------------
-    Todo: 
-        - Liste über CSV einlesen
-        - Liste als HTML, CSV, JSON oder XML ausgeben
-*/
-
-import { data, transporter, Hardware, Transporter} from './hardware';
-
-let maxCap : number = 0;
-data.forEach((d, i) => {
-    let val = d.amountNeed / d.needInt;
-    val = val * d.weight;
-    console.log(val);
-})
-console.log(maxCap); 
