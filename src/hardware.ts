a165afe1c91049311b07a4832d8a72a3c2344aa6
Christopher Mogler <christopher@mogler.dev>
2021-01-07 08:34:18 +0100

    Names from Var changed

--- src/hardware.ts
+++ src/hardware.ts
@@ -19,89 +19,92 @@ export type Hardware = {
     /** Hardware Bezeichnung */
     name: string,
     /** benötigte Anzahl Einheiten in Bonn */
-    amountNeed: number,
+    orderQuantity: number,
     /** Gewicht (mit Verpackung und Zubehör), in g */
     weight: number,
     /** Nutzwert je Hardware-Einheit (hoch=besser) */ 
-    needInt: number
+    useValue: number,
+    /** Schon versendet (wenn nichts versendet wurde ist der Wert => undefined) */
+    receivedQuantity?: number, 
 };
 
 /** Liste der benötigten Hardware mit Kapazitäten und Nutzwerte */
 export const data: Hardware[] = [
     {
         name: "Notebook Büro 13\"",
-        amountNeed: 205,
+        orderQuantity: 205,
         weight: 2451,
-        needInt: 40
+        useValue: 40
     },
     {
         name: "Notebook Büro 14\"",
-        amountNeed: 420,
+        orderQuantity: 420,
         weight: 2978,
-        needInt: 35
+        useValue: 35
     },
     {
         name: "Notebook outdoor",
-        amountNeed: 450,
+        orderQuantity: 450,
         weight: 3625,
-        needInt: 80
+        useValue: 80
     },
     {
         name: "Mobiltelefon Büro",
-        amountNeed: 60,
+        orderQuantity: 60,
         weight: 717,
-        needInt: 30
+        useValue: 30
     },
     {
         name: "Mobiltelefon Outdoor",
-        amountNeed: 157,
+        orderQuantity: 157,
         weight: 988,
-        needInt: 60
+        useValue: 60
     },
     {
         name: "Mobiltelefon Heavy Duty",
-        amountNeed: 220,
+        orderQuantity: 220,
         weight: 1220,
-        needInt: 65
+        useValue: 65
     },
     {
         name: "Tablet Büro klein",
-        amountNeed: 620,
+        orderQuantity: 620,
         weight: 1405,
-        needInt: 40
+        useValue: 40
     },
     {
         name: "Tablet Büro groß",
-        amountNeed: 250,
+        orderQuantity: 250,
         weight: 1455,
-        needInt: 40
+        useValue: 40
     },
     {
         name: "Tablet outdoor klein",
-        amountNeed: 540,
+        orderQuantity: 540,
         weight: 1690,
-        needInt: 45
+        useValue: 45
     },
     {
         name: "Tablet outdoor groß",
-        amountNeed: 370,
+        orderQuantity: 370,
         weight: 1980,
-        needInt: 68
+        useValue: 68
     }
 ];
 
 export type Transporter = {
     driverWeight: number,
-    maxCapacity: number
+    maxCapacity: number,
+    filledCapacity?: number;
 };
 
 export const transporter : Transporter[] = [
     {
         driverWeight: 85.7 * 1000,
-        maxCapacity: 1100 * 1000
+        maxCapacity: 1100 * 1000 - 85.7 * 1000
     },
     {
         driverWeight: 72.4 * 1000,
-        maxCapacity: 1100 * 1000
+        maxCapacity: 1100 * 1000 - 72.4 * 1000
     }
 ]
