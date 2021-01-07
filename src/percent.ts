ab5598e10eeb1dedc47427e526af31a50761b45a
Christopher Mogler <christopher@mogler.dev>
2021-01-07 08:34:35 +0100

    current Code outsourced

--- src/percent.ts
+++ src/percent.ts
@@ -14,3 +14,62 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
+import { data, transporter, Hardware, Transporter} from './hardware'; 
+
+const collection: Hardware[] = data.sort((item, nextItem) => {
+    return nextItem.useValue - item.useValue;
+});
+
+let sumuseValue = 0, useValue = 0;
+let countItems = 0;
+let sumWeight = 0;
+
+collection.forEach((d, i) => {
+    sumuseValue += d.useValue * d.orderQuantity;
+    useValue += d.useValue;
+    countItems += d.orderQuantity;
+    sumWeight += d.weight;
+});
+
+let usedNeedInt = 0;
+
+/**
+ * Hier wird der einzelne Transporter verarbeitet. 
+ * @param tra aktueller Transporter, der "Beladen" werden soll.
+ * @param index aktuller Index in der "for"-Schleife, vom Transporter-Array
+ */
+const spreadTransport = (tra: Transporter, index: number) => {
+    console.log("\r\nTransporter ", index + 1);
+    
+    tra.filledCapacity = 0;
+    collection.forEach((item, i) => collectItems(item, tra, i));
+
+    console.log("Max. Cap:", tra.maxCapacity / 1000, "\tSum. Cap:", tra.filledCapacity / 1000, "\tFree Cap:", (tra.maxCapacity - tra.filledCapacity) / 1000);
+}
+
+/**
+ * Hier ist der eigentliche Alg. der die Hardware zu den Transportern verteillt.
+ * @param item Hardware die aufgeteilt werden soll
+ * @param tra Transporter der "Beladen" wird 
+ * @param index Index im Array der ausgewählten Hardware
+ */
+const collectItems = (item: Hardware, tra: Transporter, index: number) => {
+    let pro = (item.useValue * item.orderQuantity) / sumuseValue; // Prozentualen Anteil der Summe vom Nutzwert
+    let use = Math.floor(pro * tra.maxCapacity);
+
+    let amount = Math.floor(use / item.weight);
+    console.log(use / 1000, amount, item.weight / 1000)
+
+    console.log(item.orderQuantity, "\t- ", amount, "\t= ", item.orderQuantity - amount, "\t", item.name);
+
+    tra.filledCapacity += amount * item.weight; 
+    usedNeedInt += amount * item.useValue;    
+    item.orderQuantity -= amount;
+}
+
+console.log("d.orderQuantity \t amount \t d.orderQuantity - amount \t d.name");
+
+transporter.forEach(spreadTransport);
+
+console.log("\r\nMax. Nutzwert:", sumuseValue, "\tSum. Nutzwert:", usedNeedInt, "\t", (usedNeedInt / sumuseValue * 100).toFixed(2), "%");
